70eb59b824ae046a0c1d815b2f0587047ddf4281
Denise Neustadt <dneustadt@auntap.com>
2022-01-18 11:47:28 -0300

    feat: add libraries and configuration for google signin

---
+++ src/public/screens/Login.screen.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from "react";
+import { Text, View } from "react-native";
+
+export const LoginScreen: FC<any> =() => {
+return (
+    <View>
+        <Text>holi</Text>
+    </View>
+)
+}
