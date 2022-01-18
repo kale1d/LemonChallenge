9673184873530353dc5d0ed1b9eadfabdf879372
Denise Neustadt <dneustadt@auntap.com>
2022-01-18 09:58:45 -0300

    first commit

---
+++ ios/Pods/Target Support Files/Pods-Covid19Cases-Covid19CasesTests/Pods-Covid19Cases-Covid19CasesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Covid19Cases_Covid19CasesTests : NSObject
+@end
+@implementation PodsDummy_Pods_Covid19Cases_Covid19CasesTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-Covid19Cases/Pods-Covid19Cases-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Covid19Cases : NSObject
+@end
+@implementation PodsDummy_Pods_Covid19Cases
+@end
